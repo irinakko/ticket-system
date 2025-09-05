319058963028a634bdfb38b517ac5b89d2e50c57
irinakko <irena.kovalenko@lento.eu>
2025-09-05 16:13:07 +0200

    feat: dashboard for admin

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CommentController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LabelController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TicketController;
@@ -33,8 +34,7 @@ Route::middleware('auth')->group(function () {
     });
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard')
+    ->middleware(['auth', 'verified']);
 
 require __DIR__.'/auth.php';
